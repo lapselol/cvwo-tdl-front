9dc98081f03f980dd5b60d246d952b70d5ae65cf
lapselol <lapselol@gmail.com>
2022-01-25 15:13:45 +0800

    improve JWT authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import axios from "axios";
 
@@ -8,45 +8,33 @@ import Dashboard from "./Dashboard";
 import Registration from "./components/authentication/Registration";
 
 export default function App() {
-  const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
+  const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
   const [loginStatus, setLoginStatus] = useState("LOGGED_IN");
   const [user, setUser] = useState({});
 
-  const checkLoginStatus = () => {
-    axios
-      .get(`${API_ENDPOINT}/logged_in`, { withCredentials: true })
-      .then((response) => {
-        if (response.data.logged_in && loginStatus === "NOT_LOGGED_IN") {
-          setLoginStatus("LOGGED_IN");
+  const authJWT = () => {
+    if (localStorage.getItem("token")) {
+      axios
+        .get(
+          `${API_ENDPOINT}/logged_in`,
+          { withCredentials: true },
+          { headers: { Authenticate: localStorage.token } }
+        )
+        .then((response) => {
           setUser(response.data.user);
-        } else if (!response.data.logged_in && loginStatus === "LOGGED_IN") {
-          setLoginStatus("NOT_LOGGED_IN");
-          setUser({});
-        }
-      })
-      .catch((error) => {
-        console.log("check login error", error);
-      });
+          console.log(response);
+        });
+    }
   };
 
-  const authJWT = () => {
-    if(localStorage.getItem("token")) {
-      axios
-        .get(`${API_ENDPOINT}/logged_in`, { withCredentials: true }, { 'headers': { 'Authenticate': localStorage.token } })
-        .then(response => {
-          setUser(response.data.user)
-          console.log(response)
-        })
-    }
-  }
   useEffect(() => {
-    authJWT()
+    authJWT();
   }, []);
 
   const handleLogout = () => {
     setLoginStatus("NOT_LOGGED_IN");
     setUser({});
-    localStorage.removeItem("token")
+    localStorage.removeItem("token");
   };
 
   const handleLogin = (data) => {
@@ -60,9 +48,7 @@ export default function App() {
         <Route path="/" element={<Home handleLogin={handleLogin} />} />
         <Route
           path="/dashboard/*"
-          element={
-            <Dashboard handleLogout={handleLogout} />
-          }
+          element={<Dashboard handleLogout={handleLogout} authJWT = {authJWT}/>}
         />
         <Route
           path="/register"
